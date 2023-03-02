ac4e64d2da5e0dd33fcb3771c4fc01d689665e44
LexHeadko <n-odes11rus@yandex.ru>
2023-03-02 16:18:27 +0200

    initial commit

---
+++ src/main/java/pro/sky/skyprospringdemolexheadko/SkyproSpringDemoLexheadkoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringdemolexheadko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringDemoLexheadkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringDemoLexheadkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringdemolexheadko/SkyproSpringDemoLexheadkoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringdemolexheadko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringDemoLexheadkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
